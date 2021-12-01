d54a557efe756537d0062161a543222638ac8827
YadharthGC <85919766+YadharthGC@users.noreply.github.com>
2021-12-01 23:54:28 +0530

    Update flipkart.js

--- src/flipkart.js
+++ src/flipkart.js
@@ -4,50 +4,61 @@ import "./App.css";
 
 function Flipkart() {
   const [list, setlist] = useState([]);
+  const [load, setload] = useState(true);
   useEffect(async () => {
     try {
       let products = await axios.get("https://yadharthfkscrap.herokuapp.com/");
       setlist([...products.data]);
-    } catch (error) {}
+      setload(false);
+    } catch (error) {
+      setload(false);
+    }
   }, []);
 
   return (
     <div className="container-fluid" id="amazon">
-      <div>
-        <h2 style={{ textAlign: "center" }}>Flipkart Shoes</h2>
-      </div>
-      <div className="row ">
-        {list.map((obj) => {
-          return (
-            <div className="col-lg-3" id="first">
-              <div className="row">
-                <div className="col">
-                  <img src={obj.srcs} alt={obj.name} id="fkimg" />
+      {load ? (
+        <h3>loading...</h3>
+      ) : (
+        <div>
+          <div>
+            <h2 style={{ textAlign: "center" }}>Flipkart Shoes</h2>
+          </div>
+          <div className="row ">
+            {list.map((obj) => {
+              return (
+                <div className="col-lg-3" id="first">
+                  <div className="row">
+                    <div className="col">
+                      <img src={obj.srcs} alt={obj.name} id="fkimg" />
+                    </div>
+                    <div className="col" id="size">
+                      <div>
+                        <strong>{obj.name}</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> Price:</span>
+                        <strong>Rs{obj.price}/-</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> Brand:</span>
+                        <strong>{obj.type}</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> offers:</span>
+                        <strong>{obj.offers}</strong>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col" id="size">
-                  <div>
-                    <strong>{obj.name}</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> Price:</span>
-                    <strong>Rs{obj.price}/-</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> Brand:</span>
-                    <strong>{obj.type}</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> offers:</span>
-                    <strong>{obj.offers}</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Flipkart;
+
