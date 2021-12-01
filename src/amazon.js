5b69e14fca985fd3fd54d238e5857dbbb79b79a1
YadharthGC <85919766+YadharthGC@users.noreply.github.com>
2021-12-01 23:48:52 +0530

    Update amazon.js

--- src/amazon.js
+++ src/amazon.js
@@ -5,54 +5,63 @@ import "./App.css";
 
 function Amazon() {
   const [list, setlist] = useState([]);
-  const[load,setload]=useState(true)
+  const [load, setload] = useState(true);
   useEffect(async () => {
     try {
-      let products = await axios.get("https://yadharthamazonscrap.herokuapp.com/");
+      let products = await axios.get(
+        "https://yadharthamazonscrap.herokuapp.com/"
+      );
       setlist([...products.data]);
       setload(false);
     } catch (error) {
-    setload(false);
+      setload(false);
     }
   }, []);
 
   return (
     <div className="container-fluid" id="amazon">
-    {loading ? (<h3>Loading...</h3>):(<div>
-        <h2 style={{ textAlign: "center" }}>Amazon Shirts</h2>
-      </div>
-      <div className="row ">
-        {list.map((obj) => {
-          return (
-            <div className="col-lg-3" id="first">
-              <div className="row">
-                <div className="col">
-                  <img src={obj.srcs} id="card-img-top" alt={obj.name} />
+      {load ? (
+        <h3>loading...</h3>
+      ) : (
+        <div>
+          <div>
+            <h2 style={{ textAlign: "center" }}>Amazon Shirts</h2>
+          </div>
+          <div className="row ">
+            {list.map((obj) => {
+              return (
+                <div className="col-lg-3" id="first">
+                  <div className="row">
+                    <div className="col">
+                      <img src={obj.srcs} id="card-img-top" alt={obj.name} />
+                    </div>
+                    <div className="col" id="size">
+                      <div>
+                        <strong>{obj.name}</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> Price:</span>
+                        <strong>Rs{obj.price}/-</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> Brand:</span>
+                        <strong>{obj.type}</strong>
+                      </div>
+                      <div>
+                        <span style={{ color: "white" }}> offers:</span>
+                        <strong>{obj.offers}</strong>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col" id="size">
-                  <div>
-                    <strong>{obj.name}</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> Price:</span>
-                    <strong>Rs{obj.price}/-</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> Brand:</span>
-                    <strong>{obj.type}</strong>
-                  </div>
-                  <div>
-                    <span style={{ color: "white" }}> offers:</span>
-                    <strong>{obj.offers}</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>)}
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Amazon;
+
